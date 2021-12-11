7c235234f8e3ae129acb37581850a22ea8b86cad
C0M3TZZ <39180519+C0M3TZZ@users.noreply.github.com>
2021-12-11 22:56:53 +0700

    Update views.py

--- lazy2use/youtube_download/views.py
+++ lazy2use/youtube_download/views.py
@@ -83,25 +83,23 @@ def yt_highdefi(request):
         "/" + var_clip.video_id+"_"+var_qulity+".mp4"
     cmd = 'ffmpeg -y -i ' + var_video_path + ' -r ' + var_fps + ' -i ' + var_audio_path + \
         ' -strict -2 -filter:a aresample=async=1 -c:a flac -c:v copy ' + var_export
-    return HttpResponse(cmd)
-
-    # if os.path.exists(var_export) == False:
-    #     var_clip.streams.filter(resolution=var_qulity.replace("p60", 'p')).first().download(
-    #         'youtube_download/download/' + var_clip.video_id, var_clip.video_id + '.webm')
-    #     var_clip.streams.get_audio_only().download('youtube_download/download/' +
-    #                                                 var_clip.video_id, var_clip.video_id + '.weba')
-    #     cmd = 'ffmpeg -y -i ' + var_video_path + ' -r ' + var_fps + ' -i ' + var_audio_path + \
-    #         ' -strict -2 -filter:a aresample=async=1 -c:a flac -c:v copy ' + var_export
-    #     subprocess.call(cmd, shell=False)
-    # if os.path.exists(var_video_path):
-    #     os.remove(var_video_path)
-    # if os.path.exists(var_audio_path):
-    #     os.remove(var_audio_path)
-    # fr = FileResponse(open(var_export, 'rb'))
-    # fr['Content-Type'] = 'video/mp4'
-    # fr['Content-Disposition'] = 'attachment; filename=' + \
-    #     var_clip.video_id+"_"+var_qulity+".mp4"
-    # return fr
+    if os.path.exists(var_export) == False:
+        var_clip.streams.filter(resolution=var_qulity.replace("p60", 'p')).first().download(
+            'youtube_download/download/' + var_clip.video_id, var_clip.video_id + '.webm')
+        var_clip.streams.get_audio_only().download('youtube_download/download/' +
+                                                    var_clip.video_id, var_clip.video_id + '.weba')
+        cmd = 'ffmpeg -y -i ' + var_video_path + ' -r ' + var_fps + ' -i ' + var_audio_path + \
+            ' -strict -2 -filter:a aresample=async=1 -c:a flac -c:v copy ' + var_export
+        subprocess.call(cmd, shell=False)
+    if os.path.exists(var_video_path):
+        os.remove(var_video_path)
+    if os.path.exists(var_audio_path):
+        os.remove(var_audio_path)
+    fr = FileResponse(open(var_export, 'rb'))
+    fr['Content-Type'] = 'video/mp4'
+    fr['Content-Disposition'] = 'attachment; filename=' + \
+        var_clip.video_id+"_"+var_qulity+".mp4"
+    return fr
 
 def get_path(request):
     return HttpResponse(pathlib.Path().resolve())
